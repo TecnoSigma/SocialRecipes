d2cd69aa252c2cbcacccb080ce33f7e3753e2cb8
Alexandre <tecnoalpha@ig.com.br>
2015-02-16 04:01:34 -0200

    Rspec funcional!

--- spec/controllers/view_recipes_controller_spec.rb
+++ spec/controllers/view_recipes_controller_spec.rb
@@ -2,4 +2,14 @@ require 'rails_helper'
 
 RSpec.describe ViewRecipesController, type: :controller do
 
+  let(:recipecreated) {{id: 1, name_recipe: "Angu", cuisine: "Brasileira", type_food: "Principal", preferences: "Calórica", served_people: 5, time_preparation: "00:30", dificulty: "Fácil", ingredients: "Fubá, Água e Sal", directions: 'Misturar tudo', created_at: Time.now, updated_at: Time.now, image_file_name: "angu.jpg", image_content_type: "image/jpg", image_file_size: 1, image_updated_at: Time.now}}
+
+  describe "#show" do 
+    it "Should list the recipe saved" do
+      @recipe = Recipe.create!(recipecreated)
+      get :show, id: @recipe.id
+      expect(response).to have_http_status(:success)
+    end
+  end
+
 end
