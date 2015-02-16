814860f92fcf55a12999baefc7e86e6c02bd0e48
Alexandre <tecnoalpha@ig.com.br>
2015-02-15 23:10:00 -0200

    Gravação de receitas funcional. Paperclip funcional!

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,6 +4,6 @@ class Recipe < ActiveRecord::Base
   validates :dificulty, inclusion: { in: %w(Fácil Médio Difícil)}
 
   #tratamento de imagens
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "120x90#" }
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "150x150#" }
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150215230406) do
+ActiveRecord::Schema.define(version: 20150216005544) do
 
   create_table "recipes", force: :cascade do |t|
     t.string   "name_recipe"
@@ -23,9 +23,12 @@ ActiveRecord::Schema.define(version: 20150215230406) do
     t.string   "dificulty"
     t.text     "ingredients"
     t.text     "directions"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
-    t.string   "image"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
 end
