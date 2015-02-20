07fce17bb85f4ef3acdea5ac0f3a1dfe62a6a263
Alexandre <tecnoalpha@ig.com.br>
2015-02-20 00:19:17 -0200

    Refatoração de rspecs (controller).

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -4,16 +4,6 @@ RSpec.describe Recipe, type: :model do
   
 describe "validations" do
 
-  it "save a recipe if mandatory fields are present" do
-    r = Recipe.new({name:, ingredients:, bla bla bla ...})
-    expect(r.save!).not_to raise error
-  end
-
-  it "raise error if name is empty" do
-    r= REcipe.new({ingredients, bla bla bla})
-    expect(r.save!).to raise error
-  end
-
 end
 
 end
